4f452880976a56c4bdea58f781ae9cac6954177c
Daniel Pradera <Daniel Pradera>
2016-05-09 13:25:14 +0200

    Clase PhotoPost

--- PhotoPost.java
+++ PhotoPost.java
@@ -28,46 +28,127 @@ public class PhotoPost
         likes = 0;
         comments = new ArrayList<>();
     }
-
+    
+    /**
+     * Adds a like to the cont of likes
+     */
     public void like()
     {
-
+        likes++;
     }
-
+    
+    /**
+     * Remove a like to the cont of likes
+     */
     public void unLike()
     {
-
+        if (likes<0)
+            likes = 0;
+        else
+            likes--;
     }
 
+    /**
+     * Add a comment to the post
+     */
     public void addComment(String text)
     {
-
+        comments.add(text);
     }
 
+    /**
+     * Return the filename for this image
+     */
     public String getImageFile()
     {
-        String imageFile = "";
-        return imageFile;
+        return filename;
     }
 
+    /**
+     * Return the caption for this image
+     */
     public String getCaption()
     {
         return caption;
     }
 
+    /**
+     * Retrun the time to the post publising
+     */
     public long getTimeStamp()
     {
         return timestamp;
     }
 
+    /**
+     * Print for screen the post whit his information
+     */
     public void display()
     {
-
+        System.out.println("User: " + username);
+        System.out.println("Filename: " + filename);
+        System.out.println("Likes: " + likes);
+        System.out.println("Caption: " + caption);
+        if(comments.size()==0)
+            System.out.println("There are not comments.");
+        else
+        {
+            System.out.println("Comments:");
+            for(int cont=0; cont<comments.size(); cont++)
+                System.out.println(cont+1 + " " + comments.get(cont));
+        }
+        System.out.println("Sent: " + timeString(System.currentTimeMillis()) + " ago.");
     }
 
-    public long timeString(long time)
+    /**
+     * Return a String whit the text of time since the message publising
+     */
+    public String timeString(long time)
     {
-        long timeString = 0;
+        long seconds = (time-timestamp)/1000; 
+        long minutes = 0;
+        long hours = 0;
+        long days = 0;
+        long months = 0;
+        long years = 0;
+        while(seconds >= 60)
+        {
+            seconds = seconds -60;
+            minutes++;
+            while(minutes >= 60)
+            {
+                minutes = minutes -60;
+                hours++;
+                while(hours >= 24)
+                {
+                    hours = hours -24;
+                    days++;
+                    while(days >= 30)
+                    {
+                        days = days -30;
+                        months++;
+                        while(months >= 12)
+                        {
+                            months = months -12;
+                            years++;
+                        }
+                    }
+                }
+            }
+        }
+        String timeString = " ";
+        if(years > 0)
+            timeString += years + " years,";
+        else if(months > 0)
+             timeString += months + " months, ";
+        else if(days > 0)
+             timeString += days + " days, ";
+        else if(hours > 0)
+             timeString += hours + " hours, ";
+        else if(minutes > 0)
+             timeString += minutes + " minutes, ";
+        else if(seconds > 0)
+             timeString += seconds + " seconds";
         return timeString;
     }
 }
